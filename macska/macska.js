40a5d53389d48708227f6c23592cd871872b8836
NYuNYork <95426913+NYuNYork@users.noreply.github.com>
2022-11-30 10:31:29 +0100

    Add files via upload

---
+++ macska/macska.js
@@ -0,0 +1,12 @@
+let rnd = Math.floor(Math.random()*13)+1;
+let pic = "";
+if(rnd<10)
+{
+    pic = "0"+rnd+".jpg";
+}
+else
+{
+    pic = rnd+".jpg";
+}
+document.getElementById("pic").src = "img/"+rnd+".jpg";
+document.getElementById("pic").alt = rnd+".jpg";
